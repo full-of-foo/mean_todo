1e9942baf6e8b4854bfa42dfd5bacc9439f563d5
Anthony <lightweightdevelopment@gmail.com>
2014-03-02 18:33:26 +0000

    Basic ng navigation play around

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCntrl', 'NerdCntrl', 'NerdService', 'GeekCntrl', 'GeekService']);
---
+++ public/js/controllers/GeekCntrl.js
@@ -0,0 +1,5 @@
+angular.module('GeekCntrl', []).controller('GeekController', function($scope) {
+
+	$scope.tagline = 'The square root of life is pi!';
+
+});
